b78fd362a6d8f027f26f076cda6b831ca1f49511
Justin Baur <19896123+justindbaur@users.noreply.github.com>
2024-11-23 08:20:34 -0500

    Remove Logo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -64,10 +64,10 @@ const config: Config = {
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
       title: 'Justin Baur',
-      logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
-      },
+      // logo: {
+      //   alt: 'My Site Logo',
+      //   src: 'img/logo.svg',
+      // },
       items: [
         {
           type: 'docSidebar',
